d3dfc56a826c40d36cbc4322e22528973fbb07fb
Katsuki Go <go@KatsukinoMacBook-Air.local>
2025-01-13 21:12:17 +0900

    message1エンドポイントとCORS追加

--- src/main/java/com/example/base/controller/HealthController.java
+++ src/main/java/com/example/base/controller/HealthController.java
@@ -1,14 +1,21 @@
 package com.example.base.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173") // フロントエンドのURLに応じて変更
 public class HealthController {
 
     @GetMapping("/health")
     public String healthCheck() {
         System.out.println("health check is called");
         return "OK";
+    }
+
+    @GetMapping("/message1")
+    public String getMessage() {
+        return "hello2";
     }
 }
